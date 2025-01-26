ca30e17349e1bd7e0301add099155e038e9137e4
teddy <teddytilahun02@gmail.com>
2025-01-26 17:08:11 +0300

    adding the project section

---
+++ src/components/Card.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div className="flex flex-col w-full ">
+      <img src="" alt="" className="w-full h-52 bg-gray-500" />
+      <div className="mt-5">
+        <p>title</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. omnis
+          exercitationem excepturi accusamus suscipit enim doloremque reiciendis
+          mollitia quae?
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
