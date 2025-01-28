dee60569c5a0aacfdc01502a32bddded9bc18254
teddy <teddytilahun02@gmail.com>
2025-01-28 15:46:58 +0300

    change resume

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -11,7 +11,7 @@ function NavItem({ name, setIsSidebarOpen, navItem, setNavItem }) {
       }}
       className={`${
         name == navItem ? "text-blue-500" : ""
-      } relative group inline-block transition-all ease-out duration-300 pl-1 pr-1 pb-2 pt-2 text-xl ml-4`}
+      } relative group inline-block transition-all ease-out duration-300 pl-1 pr-1 pb-2 pt-2 text-xl ml-4 font-poppins`}
     >
       {name}
       <span className="absolute left-0 bottom-1 w-0 h-[5px] bg-blue-400 rounded-[16px] transition-all z-0 ease-out duration-300 group-hover:w-full group-hover:z[1] "></span>
